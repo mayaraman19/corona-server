96f0af0eb5b6e8e67dd09472b6c39b572f96629f
Maya Raman <maya.raman19@gmail.com>
2020-05-31 11:19:29 -0700

    server

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
- res.send("Hello World!");
+let projects = [];
+
+app.get("/projects", (req, res) => {
+    res.json(projects);
 })
 app.listen(port, () => {
  console.log(`Listening on port ${port}!`)
